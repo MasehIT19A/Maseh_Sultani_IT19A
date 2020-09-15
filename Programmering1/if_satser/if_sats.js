bd80289182934318502fe25c55850a07fa9cf057
MasehIT19A <maseh.sultani@elev.ga.ntig.se>
2020-09-15 14:06:26 +0200

    if

---
+++ Programmering1/if_satser/if_sats.js
@@ -0,0 +1,22 @@
+//modulusperator
+console.log(`${25%2}`);
+
+let tal= 25;
+
+//avgöra om talet är jämt eller udda 
+
+if(tal%2 === 0){
+    console.log("jämnt")
+}else{
+    console.log("udda")
+}
+
+let tal1 = prompt("Ange ett tal: ")
+
+if(tal1 > 0){
+    console.log("positive")
+}else{ if(tal1 == 0){
+    console.log("talet är null")
+}else
+    console.log("negativ")
+}
---
+++ Programmering1/uppgift_1.js
@@ -0,0 +1,10 @@
+console.log("Information");
+let name = prompt ("Vad heter du?");
+let ålder = prompt ("Hur gamal är du=? ");
+let adress = prompt ("Var bor du? ");
+let mobilnummer = prompt ("Vad har du för Mobilnummer? ");
+
+console.log(`Name: ${name}`)
+console.log(`ålder: ${ålder}`)
+console.log(`adress: ${adress}`)
+console.log(`Mobilnummer: ${mobilnummer}`)
