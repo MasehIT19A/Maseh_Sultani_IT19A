c044558930aa9a69042302e0edef5f6eb2a51047
MasehIT19A <maseh.sultani@elev.ga.ntig.se>
2020-10-01 09:58:18 +0200

    uppgiften är klart

--- Programmering1/For_sats/for_uppgift.js
+++ Programmering1/For_sats/for_uppgift.js
@@ -1,11 +1,27 @@
-let jämna=document.querySelector(".jämna");
+let jamna=document.querySelector(".jamna");
 let udda=document.querySelector(".udda");
 let lektioner=document.querySelector(".lektioner");
 
-
+jamna.innerHTML = "Jämna tal från 0 till 100: ";
 for(let i=1; i<=100; i++){
-    if(i/2==0){
-        jämna.innerHTML += (`${i}`);
+    if( i % 2==0){
+    jamna.innerHTML += `${i},`;
     }
 }
-   
+udda.innerHTML = "Udda tal från 0 till 100:"
+for(let j=1; j<=100; j++){
+    if(j % 2==1){
+        udda.innerHTML += `${j},`
+    }
+}
+
+const lek = [`Svenska`,`Engelska`,`Programmering`, `Matematik`, `Idrott`];
+
+lektioner.innerHTML = ("Mina lektioner är: <br> ");
+
+let index = 1;
+
+for(name  of lek){
+    lektioner.innerHTML += `${index} ${name} <br>`;
+    index++;
+}
