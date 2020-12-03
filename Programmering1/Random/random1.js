8809b5ce8e9af494d6ccb388842b769a9c5686d8
MasehIT19A <maseh.sultani@elev.ga.ntig.se>
2020-12-03 09:50:34 +0100

    done

---
+++ Programmering1/Random/random1.js
@@ -0,0 +1,28 @@
+let slumptal=Math.random()
+
+console.log(slumptal)
+
+let coinflip = Math.round(slumptal)
+
+if(coinflip==0){
+    console.log(`coin flip är 0: ${coinflip}`)
+}else{
+    console.log(`coin flip är 1: ${coinflip}`)
+}
+
+
+let count=1
+let sex=0
+let sannolikhet=0
+
+while(count<=100){
+    let tärning=Math.floor(Math.random() * 9)
+    console.log(tärning)
+    if(tärning==6){
+        sex++
+    }
+    count++
+}
+sannolikhet=6/count
+console.log(`antalet sexor: ${sex}`)
+console.log(`sannolikheten 6 är : ${sannolikhet}`)
